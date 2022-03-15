c8d0eedb4c006389cdd8225656a6798be310fd04
nicenathapong <nicenathapong@gmail.com>
2022-03-15 14:50:55 +0700

    Remove dist folder

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nSysNode = exports.nSysManager = void 0;
-var Manager_1 = require("./Manager");
-Object.defineProperty(exports, "nSysManager", { enumerable: true, get: function () { return Manager_1.nSysManager; } });
-var Node_1 = require("./Node");
-Object.defineProperty(exports, "nSysNode", { enumerable: true, get: function () { return Node_1.nSysNode; } });
